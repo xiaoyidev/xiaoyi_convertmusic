4163118e62d1a25d4ae0a26b76e93d66423f8f4c
kenbaro <nptkeii@gmail.com>
2023-05-14 19:03:14 +0700

    xiaoyi add modal

--- script.js
+++ script.js
@@ -2,18 +2,30 @@
 // get input 
 $(document).ready(function() {
     $("#result").hide()
+    $(".modal-xiaoyi").hide();
     $("#btn-get").click(function(){
         let link=$("#txt-input").val()
         let api="https://convert2mp3s.com/api/widgetv2?url="
+        if(link !== ""){
+            $("#widgetv2Api").prop({
+                "width":"100%",
+                "height":"100%",
+                "min-height":"800px",
+                "scrolling":"no",
+                "src":api+link,
+            })
+            iFrameResize({ log: false, minHeight: 400 }, '#widgetv2Api')
+            $("#result").show()
+        } else {
+            
+            $(".xiaoyi-container").addClass("faded-xiaoyi")
+            $(".modal-xiaoyi").show();
+        }
+    });
 
-        $("#widgetv2Api").prop({
-            "width":"100%",
-            "height":"100%",
-            "min-height":"800px",
-            "scrolling":"no",
-            "src":api+link,
-        })
-        iFrameResize({ log: false, minHeight: 400 }, '#widgetv2Api')
-        $("#result").show()
-      });
+    $("#btn-ok").click(function(){
+        $("#txt-input").focus()
+        $(".xiaoyi-container").removeClass("faded-xiaoyi")
+        $(".modal-xiaoyi").hide();
+    });
 });
